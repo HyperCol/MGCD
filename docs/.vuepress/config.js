a4b1c0eac6e26200077ca12291cbc6037c4b0674
17TheWord <17theword@gmail.com>
2022-11-29 20:49:11 +0800

    新增"歧义及观点纠正"

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,16 +2,16 @@ import { defaultTheme } from '@vuepress/theme-default'
 
 module.exports = {
   lang: 'zh-CN',
-  title: 'MGC Docs',
+  title: 'correct Docs',
   // base: "/MCGD/",
   description: 'Minecraft Graphic Community Documents',
   port: "8080",
   head: [
-    ["link", { rel: "icon", href: "/images/MGC-logo.png" }],
+    ["link", { rel: "icon", href: "/images/correct-logo.png" }],
   ],
   theme: defaultTheme({
-    logo: '/images/MGC-logo.png',
-    logoDark: '/images/MGC-darklogo.png',
+    logo: '/images/correct-logo.png',
+    logoDark: '/images/correct-darklogo.png',
     notFound: ["404 Not Found，页面丢失。"],
     backToHome: "<-回到首页",
     repo: 'HyperCol/hypercol.github.io',
@@ -27,6 +27,10 @@ module.exports = {
       {
         text: '大众科普',
         link: '/science/',
+      },
+      {
+        text: 'correct',
+        link: '/correct/',
       },
     ],
 
@@ -46,7 +50,18 @@ module.exports = {
         "/science/render.md",
         "/science/film.md",
       ],
-
+      '/correct/': [
+        '/correct/README.md',
+        {
+          text: '问题集锦',
+          collapsible: true,
+          children: ['/correct/qas/shadow', '/correct/qas/texture'],
+        },
+        '/correct/mistakes',
+        '/correct/other',
+        '/correct/netease_shader',
+        '/correct/author_says',
+      ],
     },
   })
 }
