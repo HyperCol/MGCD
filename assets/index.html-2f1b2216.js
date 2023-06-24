e8aa98c66156ba5213bda0a8239162484f2d95eb
17TheWord <54731914+17TheWord@users.noreply.github.com>
2023-06-24 12:04:02 +0000

    Deploying to gh-pages from  @ c51e9f11fbee2633847e7232a942bbd102caf69c 🚀

---
+++ assets/index.html-2f1b2216.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-61807d49","path":"/science/correct/","title":"导言","lang":"zh-CN","frontmatter":{},"headers":[],"git":{"updatedTime":1687608187000,"contributors":[{"name":"17TheWord","email":"54731914+17TheWord@users.noreply.github.com","commits":1}]},"filePathRelative":"science/correct/README.md"}');export{e as data};
