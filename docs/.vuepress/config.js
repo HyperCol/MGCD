52c7a89c2625e59e2d7fb580e247baa7f7126788
shaoxiu <shaoxiu@skiesworld.com>
2023-06-25 12:02:35 +0800

    katex公式语法支持测试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 import { defaultTheme } from '@vuepress/theme-default'
 import { docsearchPlugin } from '@vuepress/plugin-docsearch'
-import {registerComponentsPlugin} from "@vuepress/plugin-register-components";
-import {path} from '@vuepress/utils'
+import { registerComponentsPlugin } from "@vuepress/plugin-register-components";
+import { path } from '@vuepress/utils'
 
 module.exports = {
   lang: 'zh-CN',
@@ -62,6 +62,16 @@ module.exports = {
             },
           },
         },
+        markdown: {
+          // markdown-it-anchor 的选项
+          anchor: { permalink: false },
+          // markdown-it-toc 的选项
+          toc: { includeLevel: [1, 2] },
+          extendMarkdown: md => {
+            // 使用更多的 markdown-it 插件!
+            md.use(require('markdown-it-katex'))
+          }
+        }
       },
     ),
     registerComponentsPlugin(
