73a9dda0de7babc6535961d57be9690f3ae7442e
shaoxiu <shaoxiu@skiesworld.com>
2023-06-25 12:08:44 +0800

    katex公式支持

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -63,12 +63,7 @@ module.exports = {
           },
         },
         markdown: {
-          // markdown-it-anchor 的选项
-          anchor: { permalink: false },
-          // markdown-it-toc 的选项
-          toc: { includeLevel: [1, 2] },
           extendMarkdown: md => {
-            // 使用更多的 markdown-it 插件!
             md.use(require('markdown-it-katex'))
           }
         }
