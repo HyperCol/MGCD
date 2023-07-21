50d441331942c6afe978437f7fc56717917dcd6b
shaoxiu <shaoxiu@skiesworld.com>
2023-07-21 14:12:38 +0800

    更新vuepress版本

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -64,16 +64,6 @@ module.exports = {
                     },
                 },
             },
-            markdown: {
-                // markdown-it-anchor 的选项
-                anchor: { permalink: false },
-                // markdown-it-toc 的选项
-                toc: { includeLevel: [1, 2] },
-                extendMarkdown: md => {
-                    // 使用更多的 markdown-it 插件!
-                    md.use(require('markdown-it-katex'))
-                }
-            }
         }),
         registerComponentsPlugin({ componentsDir: path.resolve(__dirname, './components') }),
     ],
