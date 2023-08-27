6b3292668e197e37bfcc1bb681dbd09f312d10bd
shaoxiu <50393917+shaoxiu@users.noreply.github.com>
2023-08-27 09:47:34 +0000

    Deploying to gh-pages from  @ 66fb3c6f47002fe0ce6c98a89cc5d4b3b55600a9 🚀

---
+++ assets/bedrock_resourcepacks.html-fc83d75e.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-25b8694c","path":"/works/bedrock_resourcepacks.html","title":"基岩版资源包","lang":"zh-CN","frontmatter":{},"headers":[],"git":{"updatedTime":1693129593000,"contributors":[{"name":"ShaoXiu","email":"50393917+shaoxiu@users.noreply.github.com","commits":1}]},"filePathRelative":"works/bedrock_resourcepacks.md"}');export{e as data};
