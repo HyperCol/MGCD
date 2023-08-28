a366003d4eb9f0b433da09fd183ef175c865379e
shaoxiu <50393917+shaoxiu@users.noreply.github.com>
2023-08-28 07:22:05 +0000

    Deploying to gh-pages from  @ 249734e53de5c62525c84d934d53dbe014274b03 🚀

---
+++ assets/java_resourcepacks.html-7d2274e7.js
@@ -0,0 +1,1 @@
+const a=JSON.parse('{"key":"v-04d64784","path":"/works/java_resourcepacks.html","title":"Java版资源包","lang":"zh-CN","frontmatter":{},"headers":[],"git":{"updatedTime":1693207250000,"contributors":[{"name":"ShaoXiu","email":"50393917+shaoxiu@users.noreply.github.com","commits":1}]},"filePathRelative":"works/java_resourcepacks.md"}');export{a as data};
