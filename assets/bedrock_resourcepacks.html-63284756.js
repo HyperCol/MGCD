19034c1503f2d58af4fe9bdc32ab0552bd971276
17TheWord <54731914+17TheWord@users.noreply.github.com>
2023-08-31 06:24:07 +0000

    Deploying to gh-pages from  @ fc793aa8a1cb0a7f355bd87e04f3535e32a9be05 🚀

--- assets/bedrock_resourcepacks.html-63284756.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-25b8694c","path":"/works/bedrock_resourcepacks.html","title":"基岩版资源包","lang":"zh-CN","frontmatter":{},"headers":[],"git":{"updatedTime":1693207334000,"contributors":[{"name":"ShaoXiu","email":"50393917+shaoxiu@users.noreply.github.com","commits":1}]},"filePathRelative":"works/bedrock_resourcepacks.md"}');export{e as data};
